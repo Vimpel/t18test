705f937508ba2cd9f1b80e9f4db8ae39fb79bee3
uaa <uaa@localhost.localdomain>
2016-07-21 00:21:38 +0400

    add config file, translate folder with ru.php, add admin /controllers/admin/admint18test.php

--- t18test.php
+++ t18test.php
@@ -1,24 +1,51 @@
-<?php   
+<?php
 if (!defined('_PS_VERSION_'))
   exit;
+ 
 class T18Test extends Module {
-	public function __construct() {
-	    $this->name = 't18test';
-	    $this->tab = 'administration';
-	    $this->version = '0.0.1';
-	    $this->author = 'Vimpel';
-	    $this->need_instance = 0;
-	    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_); 
-	    $this->bootstrap = true;
-	 
-	    parent::__construct();
-	 
-	    $this->displayName = $this->l('T18 test module');
-	    $this->description = $this->l('T18 first test module.');
-	 
-	    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
-	 
-	    // if (!Configuration::get('t18test'))      
-	    //   $this->warning = $this->l('No name provided');
-  	}
+    
+    public function __construct()
+    {
+        $this->name = 't18test';
+        $this->tab = 'administration';
+        $this->version = '0.0.1';
+        $this->author = 'Vimpel';
+        $this->need_instance = 1; // 0 for disable warnings
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6'); 
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('The T18 test module');
+        $this->description = $this->l('This module is startup for other modules');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!Configuration::get('t18test'))      
+          $this->warning = $this->l('No name provided');
+    }
+  
+    public function install() {
+        if (!parent::install())
+          return false;
+        return true;
+    }
+    
+    public function unistall() {
+        if (!parent::uninstall())
+        return false;
+        return true;
+    }
+    protected function installController($controllerName, $name) {
+        $tab_admin_order_id = Tab::getIdFromClassName('AdminTools');
+        $tab = new Tab();
+        $tab->class_name = $controllerName;
+        $tab->id_parent = $tab_admin_order_id;
+        $tab->module = $this->name;
+        $languages = Language::getLanguages(false);
+        foreach($languages as $lang){
+            $tab->name[$lang['id_lang']] = $name;
+        }
+    	$tab->save();
+    }
 }
---
+++ translations/ru.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{t18test}prestashop>t18test_68ffdea211351fb80c390da09dd10613'] = 'T18 тестовый модуль';
+$_MODULE['<{t18test}prestashop>t18test_e9577034e60f00407174b8077c488a6e'] = 'Это стартовый модуль для создания других модулей';
+$_MODULE['<{t18test}prestashop>t18test_876f23178c29dc2552c0b48bf23cd9bd'] = 'Вы уверены что хотите отключить модуль?';
+$_MODULE['<{t18test}prestashop>t18test_0f40e8817b005044250943f57a21c5e7'] = 'Имя не обнаружено';
